675cb78f2e62e5c893f473e7d4fcc9c456d25ef8
Alyona <alyona.dux@yandex.ru>
2022-06-29 18:30:54 +0300

    добавлено свойство title

--- src/app/ui/checkbox/checkbox.component.ts
+++ src/app/ui/checkbox/checkbox.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class CheckboxComponent implements OnInit {
   @Input() disabled = false;
+  @Input() title = 'Сахарозаменитель';
 
   constructor() { }
 
