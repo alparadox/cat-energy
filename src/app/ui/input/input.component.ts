d17f8e3b5eeaf9974adb68e36ddf7749266fbd83
Alyona <alyona.dux@yandex.ru>
2022-06-30 15:54:20 +0300

    верстка компонент input wip

--- src/app/ui/input/input.component.ts
+++ src/app/ui/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
-  constructor() { }
+  @Input() title = "Имя:*"
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
