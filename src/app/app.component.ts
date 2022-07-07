c9acafba3ffe7f419e439fb56a30386b220122e0
Alyona <alyona.dux@yandex.ru>
2022-07-07 14:49:10 +0300

    добавлеа логика

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cat-energy';
+
+
+  necessityCards: any = [
+    {titleButton: 'каталог slim', titleCard: 'похудение', descriptionCard: 'Ваш кот весит больше собаки и почти утратил способность лазить по деревьям? Пора на диету! Cat Energy Slim поможет вашему питомцу сбросить лишний вес.', path: './assets/images/cat-slim.svg'},
+    {titleButton: 'каталог pro', titleCard: 'набор массы', descriptionCard: 'Заработать авторитет среди дворовых котов и даже собак? Серия Cat Energy Pro поможет вашему коту нарастить необходимые мышцы!', path: './assets/images/cat-pro.svg'}
+  ];
 }
+
