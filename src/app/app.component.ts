48663c7b98c9b920bc17377483a2ae7ee60d4e4a
Alyona <alyona.dux@yandex.ru>
2022-07-08 17:39:07 +0300

    necessity мобильная версия готова

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cat-energy';
 
-
   necessityCards: any = [
-    {titleButton: 'каталог slim', titleCard: 'похудение', descriptionCard: 'Ваш кот весит больше собаки и почти утратил способность лазить по деревьям? Пора на диету! Cat Energy Slim поможет вашему питомцу сбросить лишний вес.', path: './assets/images/cat-slim.svg'},
-    {titleButton: 'каталог pro', titleCard: 'набор массы', descriptionCard: 'Заработать авторитет среди дворовых котов и даже собак? Серия Cat Energy Pro поможет вашему коту нарастить необходимые мышцы!', path: './assets/images/cat-pro.svg'}
+    {
+      titleButton: 'каталог slim',
+      titleCard: 'похудение',
+      descriptionCard: 'Ваш кот весит больше собаки и почти утратил способность лазить по деревьям? Пора на диету! Cat Energy Slim поможет вашему питомцу сбросить лишний вес.',
+      path: './assets/images/cat-slim.svg'
+    },
+    {
+      titleButton: 'каталог pro',
+      titleCard: 'набор массы',
+      descriptionCard: 'Заработать авторитет среди дворовых котов и даже собак? Серия Cat Energy Pro поможет вашему коту нарастить необходимые мышцы!',
+      path: './assets/images/cat-pro.svg'
+    }
   ];
-}
 
+}
