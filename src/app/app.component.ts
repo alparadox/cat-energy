528ce1fa280ba999ff2dfde709a19bd9c854f0c2
Alyona <alyona.dux@yandex.ru>
2022-07-12 16:00:14 +0300

    подключена тайга и создана функция для расчета массы животного

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component, Inject, Input} from '@angular/core';
+import {TuiDialogService} from "@taiga-ui/core";
 
 @Component({
   selector: 'app-root',
@@ -22,5 +23,40 @@ export class AppComponent {
       path: './assets/images/cat-pro.svg'
     }
   ];
+
+  parametr1 = 30;
+  parametr2 = 15;
+  result = 'вес';
+
+  constructor(
+    @Inject(TuiDialogService) private readonly dialogService: TuiDialogService,
+  ) {}
+
+  calcWeight() {
+    let weight = ((this.parametr1 / 0.762)-this.parametr2) / 0.9156 - this.parametr2;
+    return weight;
+  }
+
+  showResultWeight() {
+    let count = this.calcWeight();
+
+    if(count >= 9 && count <= 30) {
+      this.result = 'Вес вашего питомца в пределах нормы';
+    } else if(count < 9) {
+      this.result = 'У вашего питомца недостаток веса';
+    } else if(count > 30 && count < 42) {
+      this.result = 'У вашего питомца избыточный вес';
+    } else if(count >= 42 ) {
+      this.result = 'У вашего питомца ожирение. Рекомендуем обратиться к специалисту';
+    }
+
+    return this.result;
+  }
+
+  onClickPopUp(): void {
+    this.dialogService
+    .open('This is a plain string dialog', {label: 'Heading', size: 'm'})
+    .subscribe();
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './ui/button/button.component';
@@ -24,9 +26,13 @@ import { NecessityComponent } from './necessity/necessity.component';
     NecessityComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+      TuiRootModule,
+      BrowserAnimationsModule,
+      TuiDialogModule,
+      TuiAlertModule
+],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
